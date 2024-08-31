81203b4ebab5935db9e5e1b156853127370b1600
shamilculer <shamilculer10@gmail.com>
2024-08-31 11:45:02 +0530

    updated hero bg in hero.jsx

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage : {
+        herosvg : "url(./src/assets/hero-bg.svg)"
+      },
       colors: {
         "primary-colour": "#3A0CA3",
         "color1": "#4361EE",
